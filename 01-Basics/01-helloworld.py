f7573850b526b0d1e9a28ee52b93ee9dc3748997
Sanghee <lastcanyon@gmail.com>
2017-01-16 23:44:00 +0900

    with 해당 주석 추가

--- 01-Basics/01-helloworld.py
+++ 01-Basics/01-helloworld.py
@@ -6,6 +6,7 @@ b = tf.placeholder(tf.int16)
 add = tf.add(a, b)
 mul = tf.mul(a, b)
 
+#이것은 무엇을 하는 것인가.. with..?
 with tf.Session() as sess:
     print ("Addition with variables: %i" % sess.run(add, feed_dict={a: 2, b: 3}))
     print ("Multiplication with variables: %i" % sess.run(mul, feed_dict={a: 2, b: 3}))
