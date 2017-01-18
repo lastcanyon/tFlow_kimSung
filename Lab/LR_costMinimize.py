d9e85bfa035d1676c410c561e21e0c66f561e947
Sanghee <lastcanyon@gmail.com>
2017-01-18 17:05:05 +0900

    Git Error Resolved

--- Lab/LR_costMinimize.py
+++ Lab/LR_costMinimize.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-X = [1, 2, 3]
-Y = [1, 2, 3]
+X = [1., 2., 3.]
+Y = [1., 2., 3.]
 m = n_samples = len(X)
 
 W = tf.placeholder(tf.float32)
@@ -25,6 +25,6 @@ for i in range(-30, 50):
     cost_val.append(sess.run(cost, feed_dict={W: i*0.1}))
 
 plt.plot(W_val, cost_val, 'ro')
-plt.ylable('Cost')
-plt.xlable('W')
+plt.ylabel('Cost')
+plt.xlabel('W')
 plt.show()
