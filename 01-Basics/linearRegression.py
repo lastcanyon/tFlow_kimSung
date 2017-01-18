bf4ef8807b7958894bd62671d62a300557106091
Sanghee <lastcanyon@gmail.com>
2017-01-18 16:39:21 +0900

    Cost Minimize 코드 추가. 오류 생김

--- 01-Basics/linearRegression.py
+++ 01-Basics/linearRegression.py
@@ -18,7 +18,7 @@ a = tf.Variable(0.1)
 optimizer = tf.train.GradientDescentOptimizer(a)
 train = optimizer.minimize(cost)
 
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 
 #Launch the graph
 sess = tf.Session()
