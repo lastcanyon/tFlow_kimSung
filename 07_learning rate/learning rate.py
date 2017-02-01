acc641d5deb03488d90a5fc88f71d0dde49f47c7
Sanghee <lastcanyon@gmail.com>
2017-02-01 17:01:01 +0900

    Learning rate 10.(학습X)

---
+++ 07_learning rate/learning rate.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import numpy as np
+
+xy = np.loadtxt('train.txt', unpack=True, dtype='float32')
+x_data = np.transpose(xy[0:3])
+y_data = np.transpose(xy[3:])
+
+# tf Graph Input
+X = tf.placeholder("float", [None, 3]) # x1, x2 and 1 (for bias)
+Y = tf.placeholder("float", [None, 3]) # A, B, C => 3 classes
+
+# Set model weights
+W = tf.Variable(tf.zeros([3, 3])) # [입력 데이터 3개, 예측값 3개]
+
+# Construct model
+hypothesis = tf.nn.softmax(tf.matmul(X, W))
+
+# Minimize error using cross entropy
+learning_rate = 10.0 # 학습이 일어나지않음
+
+# Cross entropy
+cost = tf.reduce_mean(-tf.reduce_sum(Y*tf.log(hypothesis), reduction_indices=1))
+
+# Gradient Descent
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+# Launch the graph
+for step in range(2001):
+    sess.run(optimizer, feed_dict={X:x_data, Y:y_data})
+    if step % 200 == 0:
+        print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W))
